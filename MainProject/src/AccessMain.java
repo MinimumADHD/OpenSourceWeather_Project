a226cd59683aed988adeddc37992f0c9001fc8de
SimoRBLX <91323541+SimoRBLX@users.noreply.github.com>
2022-12-23 09:48:16 +0100

    Update AccessMain.java

--- MainProject/src/AccessMain.java
+++ MainProject/src/AccessMain.java
@@ -1,10 +1,10 @@
 import com.google.gson.Gson;
 import java.util.Scanner;
-
 public class AccessMain
 {
     static Scanner scanner = new Scanner(System.in);
     static String InsertedLocation;
+    static String API_KEY = GitIgnoreClass.API_KEY;
     public static void main(String[] args)
     {
         System.out.println("Insert your desired weather location; ");
