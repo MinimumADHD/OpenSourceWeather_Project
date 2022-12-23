8b55d11f61dc7e9955115af794c3138a87d66e45
SimoRBLX <91323541+SimoRBLX@users.noreply.github.com>
2022-12-23 10:08:58 +0100

    Some technical adjustment

--- MainProject/src/AccessMain.java
+++ MainProject/src/AccessMain.java
@@ -1,13 +1,39 @@
 import com.google.gson.Gson;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 public class AccessMain
 {
     static Scanner scanner = new Scanner(System.in);
     static String InsertedLocation;
     static String API_KEY = GitIgnoreClass.API_KEY;
-    public static void main(String[] args)
+    public static void main(String[] args) throws InterruptedException
     {
-        System.out.println("Insert your desired weather location; ");
-        InsertedLocation = scanner.nextLine();
+        String ChooseOption;
+        while (true)
+        {
+            System.out.println("Insert your desired weather location; ");
+            InsertedLocation = scanner.nextLine();
+            System.out.println("Are you sure it's the right city? (YES/NO case sensitive)");
+            ChooseOption = scanner.nextLine();
+            switch (ChooseOption)
+            {
+                // Asks for confirmation, if yes, the while loops gets completly broken, if NO, it will ask again to re input the string.
+                case "YES":
+                {
+                    System.out.println("Confirmed.");
+                    TimeUnit.SECONDS.sleep(5);
+                    System.out.println("Working on API: "+API_KEY);
+                    return;
+                }
+                case "NO":
+                {
+                    break;
+                }
+                default:
+                {
+                    System.out.println("Unknown Option, retry.");
+                }
+            }
+        }
     }
 }
